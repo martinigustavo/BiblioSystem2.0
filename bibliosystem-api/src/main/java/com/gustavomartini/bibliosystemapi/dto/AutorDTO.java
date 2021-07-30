f355a6814e9e7103c70426abbecd3e4cf4aeed5c
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-07-29 22:05:47 -0300

    :sparkles: frontend

---
+++ bibliosystem-api/src/main/java/com/gustavomartini/bibliosystemapi/dto/AutorDTO.java
@@ -0,0 +1,2 @@
+package com.gustavomartini.bibliosystemapi.dto;public class AutorDTO {
+}
---
+++ bibliosystem-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>BiblioSystem 2.0</h1>
+      <p>Coming soon...</p>
+    </>
+  );
+}
+
+export default App;
